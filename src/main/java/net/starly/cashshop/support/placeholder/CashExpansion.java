b9fde987ce530922d5eb7f9876a3b57ddd48c645
cccgh5 <125978024+hodongwook@users.noreply.github.com>
2023-02-23 15:56:02 +0900

    add:: support classes

---
+++ src/main/java/net/starly/cashshop/support/placeholder/CashExpansion.java
@@ -0,0 +1,42 @@
+package net.starly.cashshop.support.placeholder;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import net.starly.cashshop.CashShopMain;
+import net.starly.cashshop.cash.PlayerCash;
+import net.starly.cashshop.util.FormattingUtil;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+public class CashExpansion extends PlaceholderExpansion {
+
+    private final CashShopMain plugin;
+    public CashExpansion(CashShopMain plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return "starly";
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return "ST-CashShop";
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return "1.0";
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer player, String params) {
+        if(player == null) return null;
+        if(params.equalsIgnoreCase("cash")) {
+            PlayerCash cash = plugin.getPlayerCashRepository().getPlayerCash(player);
+            if(cash == null) return null;
+            return FormattingUtil.formattingCash(cash.getCash());
+        } else return null;
+    }
+
+}
---
+++ src/main/java/net/starly/cashshop/support/skript/CashExpression.java
@@ -0,0 +1,74 @@
+package net.starly.cashshop.support.skript;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.classes.Changer;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import net.starly.cashshop.CashShopMain;
+import net.starly.cashshop.cash.PlayerCash;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+
+public class CashExpression extends SimpleExpression<Long> {
+
+    public static void registerExpression() {
+        Skript.registerExpression(CashExpression.class, Long.class, ExpressionType.PROPERTY, "cash of %player%");
+    }
+
+    private Expression<Player> player;
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult result) {
+        player = (Expression<Player>) exprs[0];
+        return true;
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public String toString(Event event, boolean debug) {
+        return player.toString(event, debug);
+    }
+
+    @Override
+    public Class<Long> getReturnType() { return Long.class; }
+
+    @Override
+    public Long[] get(Event event) {
+        Player player = this.player.getSingle(event);
+        if(player != null) return new Long[]{CashShopMain.getPlugin().getPlayerCashRepository().getPlayerCash(player.getUniqueId()).getCash()};
+        return null;
+    }
+
+    @Override
+    public Class<?>[] acceptChange(Changer.ChangeMode mode) {
+        Class<?>[] classes;
+        if(mode == Changer.ChangeMode.REMOVE || mode == Changer.ChangeMode.SET || mode == Changer.ChangeMode.ADD)
+            classes = new Class[] { Long.class };
+        else classes = null;
+        return classes;
+    }
+
+    @Override
+    public void change(Event event, Object[] delta, Changer.ChangeMode mode) {
+        Player player = this.player.getSingle(event);
+        if(player != null) {
+            PlayerCash cash = CashShopMain.getPlugin().getPlayerCashRepository().getPlayerCash(player.getUniqueId());
+            if(mode == Changer.ChangeMode.ADD) {
+                cash.setCash("스크립트 추가", PlayerCash.Type.ADD, (long) delta[0]);
+            } else if(mode == Changer.ChangeMode.REMOVE) {
+                cash.subCash("스크립트 감소", PlayerCash.Type.SUB, (long) delta[0]);
+            } else if(mode == Changer.ChangeMode.SET) {
+                cash.subCash("스크립트 설정", PlayerCash.Type.SET, (long) delta[0]);
+            }
+        }
+    }
+
+
+}
