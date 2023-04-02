1e3f9990d8e84d175ab6de7b8f1ff0f9328a1a6f
yejunho10 <hyjcompany30@naver.com>
2023-04-02 13:55:48 +0900

    fix: Bugs

--- src/main/java/net/starly/cashshop/CashShopMain.java
+++ src/main/java/net/starly/cashshop/CashShopMain.java
@@ -121,7 +121,7 @@ public class CashShopMain extends JavaPlugin {
         try {
             Class.forName(path);
             return true;
-        } catch (NoClassDefFoundError ignored) {
+        } catch (ClassNotFoundException ignored) {
         } catch (Exception ex) { ex.printStackTrace(); }
         return false;
     }
